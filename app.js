d78b46416be9299804dd48639208dc2797f07667
Matías Beraldo Fernández <matias.beraldo.work@gmail.com>
2024-02-08 23:06:48 +0100

    Experimenting with custom rate limit

--- app.js
+++ app.js
@@ -1,17 +1,44 @@
 require('module-alias/register');
 const express = require('express');
-
+const { addSeconds, isAfter } = require('date-fns');
 const verifiers = require('./verifiers');
 const badger = require('./utils/nearbadger');
 
 const app = express();
 app.use(express.json());
 
+let requests = {};
+const MAX_REQUESTS = 5;
+const MAX_TIMESPAN = 30;
+
+app.use((req, res, next) => {
+    let ip = req.ip;
+
+    if (ip in requests) {
+        if (requests[ip].count == MAX_REQUESTS && !isAfter(new Date(), requests[ip].expire)) {
+            return res.status(429).send();
+        } else if (requests[ip].count == MAX_REQUESTS) {
+            requests[ip].count = 1;
+            requests[ip].expire = addSeconds(new Date(), MAX_TIMESPAN);
+        } else {
+            ++requests[ip].count;            
+        }
+    } else {
+        requests[ip] = {
+            count: 1,
+            expire: addSeconds(new Date(), MAX_TIMESPAN)
+        };
+    }
+
+    return next();
+});
+
 app.get('/ping', (req, res) => {
   return res.status(200).json({"ping": "pong"});
 });
 
 app.post('/verify/:platform', async (req, res) => {
+    console.log(requests);
   const { platform } = req.params;
   const { accountId, handle, proof } = req.body;
 
