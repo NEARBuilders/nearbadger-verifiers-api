06de2290fcffab97fabee39d1587c6deac7819b7
Matías Beraldo Fernández <matias.beraldo.work@gmail.com>
2024-02-11 00:31:43 +0100

    Adde full integration by using Warpcast endpoint to get verifications combined with farcaster fnames endpoint to get handle fid

--- utils/farcaster.js
+++ utils/farcaster.js
@@ -1,24 +1,61 @@
-const FARCASTER_MAINNET_URL = "https://searchcaster.xyz/api";
-const PROFILE_ENDPOINT = "profiles";
+const WARPCAST_API_URL = "https://client.warpcast.com/v2";
+const FARCASTER_FNAMES_API_URL = "https://fnames.farcaster.xyz";
+
+const PROFILE_ENDPOINT = "verifications";
+const FARCASTER_FNAMES_ENDPOINT = "transfers/current";
 
 export class FarcasterAPI {
     async getHandleOwner(handle) {
-        return fetch(this.getQueryURL(PROFILE_ENDPOINT, { username: handle }), {
-           method: "GET",
-           headers: {
-               Accept: "application/json"
-           }
-        }).then((payload) => {
-            return payload.json();
-        }).then((result) => {
-            console.log(result);
-            return result?.connectedAddress;
+        return this.getFidByHandle(handle).then(async (fid) => {
+            if (!fid) {
+                return [];
+            }
+
+            return fetch(
+                this.getWarpcastQueryURL(PROFILE_ENDPOINT, { fid }),
+                {
+                    method: "GET",
+                    headers: {
+                        Accept: "application/json"
+                    }
+                }
+             ).then((payload) => {
+                return payload.json();
+             }).then((result) => {
+                const verifications = result?.result?.verifications || [];
+                return verifications.map((verification) => verification.address);
+             })
         });
     }
-    getQueryURL(endpoint, params) {
-        return `${this.getURL(endpoint)}?${(new URLSearchParams(params)).toString()}`
+    async getFidByHandle(handle) {
+        return fetch(this.getFarcasterQueryURL(FARCASTER_FNAMES_ENDPOINT, { name: handle }), {
+            method: "GET",
+            headers: {
+                Accept: "application/json"
+            }
+         }).then((payload) => {
+            return payload.json();
+         }).then((result) => {
+            return result?.transfer?.to || "";
+         });
     }
-    getURL(endpoint) {
-        return `${FARCASTER_MAINNET_URL}/${endpoint}`;
+    getQueryURL(endpoint, params) {
+        return `${endpoint}?${(new URLSearchParams(params)).toString()}`
+    }
+    getFarcasterQueryURL(endpoint, params) {
+        return this.getFarcasterURL(
+            this.getQueryURL(endpoint, params)
+        );
+    }
+    getWarpcastQueryURL(endpoint, params) {
+        return this.getWarpcastURL(
+            this.getQueryURL(endpoint, params)
+        );
+    }
+    getFarcasterURL(endpoint) {
+        return `${FARCASTER_FNAMES_API_URL}/${endpoint}`;
+    }
+    getWarpcastURL(endpoint) {
+        return `${WARPCAST_API_URL}/${endpoint}`;
     }
 };
--- verifiers/farcaster.js
+++ verifiers/farcaster.js
@@ -5,8 +5,10 @@ export default class FarcasterVerifier extends AbstractVerifier {
   async verify(accountId, handle, proof) {
     const api = new FarcasterAPI();
     const challenge = this.getChallenge(accountId, handle);
-    const expectedSigner = await api.getHandleOwner(handle);
+    const expectedSigners = await api.getHandleOwner(handle);
     
-    return expectedSigner?.toLowerCase() === this.getSignerAddress(challenge, proof).toLowerCase();
+    return expectedSigners.filter(
+      (expectedSigner) => expectedSigner.toLowerCase() === this.getSignerAddress(challenge, proof).toLowerCase()
+    ).length > 0;
   }
 }
