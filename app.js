2a3d953767971eb983d3acfd41e31cc42bab13a1
Matías Beraldo Fernández <matias.beraldo.work@gmail.com>
2024-02-14 18:39:40 +0100

    Feature: Add CORS policy

--- app.js
+++ app.js
@@ -1,36 +1,13 @@
 import express from 'express';
-import { addSeconds, isAfter } from 'date-fns';
+import cors from 'cors';
 import verifiers from './verifiers/index.js';
 import badger from './utils/nearbadger.js';
+import * as config from './config.js';
 
 const app = express();
 app.use(express.json());
-
-let requests = {};
-const MAX_REQUESTS = 50;
-const MAX_TIMESPAN = 3600;
-
-app.use((req, res, next) => {
-    let ip = req.ip;
-
-    if (ip in requests) {
-        if (requests[ip].count === MAX_REQUESTS && !isAfter(new Date(), requests[ip].expire)) {
-            return res.status(429).send();
-        } else if (requests[ip].count === MAX_REQUESTS) {
-            requests[ip].count = 1;
-            requests[ip].expire = addSeconds(new Date(), MAX_TIMESPAN);
-        } else {
-            ++requests[ip].count;            
-        }
-    } else {
-        requests[ip] = {
-            count: 1,
-            expire: addSeconds(new Date(), MAX_TIMESPAN)
-        };
-    }
-
-    return next();
-});
+app.use(cors(config.cors))
+app.use(config.rateLimit);
 
 app.get('/ping', (req, res) => {
   return res.status(200).json({"ping": "pong"});
